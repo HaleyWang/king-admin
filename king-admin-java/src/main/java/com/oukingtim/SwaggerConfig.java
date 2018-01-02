7b7760bc9f0b269b9695acd867586eee55b74d49
wang3065 <0400150222@163.com>
2018-01-02 23:13:43 +0800

    add swagger

---
+++ king-admin-java/src/main/java/com/oukingtim/SwaggerConfig.java
@@ -0,0 +1,65 @@
+package com.oukingtim;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * SwaggerConfig
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket v1Api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("v1")
+                .genericModelSubstitutes(DeferredResult.class)
+//                .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(true)
+                .pathMapping("/")
+                .select()
+                .paths(or(regex("/*.*")))
+                .build()
+                .apiInfo(apiInfo("v1"));
+    }
+
+    @Bean
+    public Docket v2Api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("v2")
+                .genericModelSubstitutes(DeferredResult.class)
+//              .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(false)
+                .pathMapping("/")
+                .select()
+                .paths(or(regex("/v2/*.*")))
+                .build()
+                .apiInfo(apiInfo("v2"));
+    }
+
+    private ApiInfo apiInfo(String version) {
+        Contact contact = new Contact("-----@qq.com", "", "");
+        ApiInfo apiInfo = new ApiInfo("baton api",
+                "baton api, all the applications could access the Object model data via JSON.",
+                version,
+                "NO terms of service",
+                contact,
+                "The Apache License, Version xxxx",
+                "http://www.xxxxx.org/licenses/LICENSE-2.0.html"
+        );
+
+        return apiInfo;
+    }
+}
